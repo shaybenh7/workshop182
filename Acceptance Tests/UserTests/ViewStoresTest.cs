def2aec6cfadab26888249b455843dbd9269d0fc
aviad31 <mikud40@gmail.com>
2018-04-18 10:34:06 +0300

    add tests to view products and stores

--- Acceptance Tests/UserTests/ViewStoresTest.cs
+++ Acceptance Tests/UserTests/ViewStoresTest.cs
@@ -39,6 +39,27 @@ namespace Acceptance_Tests.UserTests
         }
 
         [TestMethod]
+        public void ViewMultipuleStores()
+        {
+            userServices us = userServices.getInstance();
+            User session = us.startSession();
+            us.register(session, "zahi", "123456");
+            us.login(session, "zahi", "123456");
+            User aviad = us.startSession();
+            us.register(aviad, "aviad", "123456");
+            us.login(aviad, "aviad", "123456");
+            storeServices ss = storeServices.getInstance();
+            Store store = ss.createStore("abowim", session);
+            Store store2 = ss.createStore("abowim2", session);
+            Store store3 = ss.createStore("bro's", aviad);
+            LinkedList<Store> Lstore = us.viewStores();
+            Assert.IsTrue(Lstore.Contains(store));
+            Assert.IsTrue(Lstore.Contains(store2));
+            Assert.IsTrue(Lstore.Contains(store3));
+            Assert.AreEqual(Lstore.Count, 3);
+        }
+
+        [TestMethod]
         public void ViewStoreWhenThrerIsNoStores()
         {
             userServices us = userServices.getInstance();
